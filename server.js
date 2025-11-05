b06412a66d09f9ba10ffc355ba6cb3e471f22550
drop2653 <30587523+drop2653@users.noreply.github.com>
2025-11-06 02:11:10 +0900

    Update server.js

--- server.js
+++ server.js
@@ -15,15 +15,18 @@ app.use(express.static(path.join(__dirname, 'public'))); // public 폴더에 ind
 
 // 사용자 연결 처리
 io.on('connection', (socket) => {
-  console.log('연결됨:', socket.id);
-  socket.emit('init', { id: socket.id });
+  const startX = 200 + Math.random() * 1600;
+  const startY = 200 + Math.random() * 1100;
 
-  socket.on('move', (data) => {
-    socket.broadcast.emit('opponentMove', data);
+  socket.emit("init", { id: socket.id, x: startX, y: startY });
+
+  socket.on("move", (data) => {
+    // 상대방에게만 전송
+    socket.broadcast.emit("opponentMove", { ...data, id: socket.id });
   });
 
-  socket.on('fire', (data) => {
-    socket.broadcast.emit('enemyFire', data);
+  socket.on("fire", (data) => {
+    socket.broadcast.emit("enemyFire", { ...data, id: socket.id });
   });
 });
 
@@ -32,4 +35,5 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`✅ 서버 실행중: http://localhost:${PORT}`);
 });
+
 
